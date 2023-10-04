938a03340d93fe410487b2638e7b123b3b9001d8
Abigiel <abigailtwg@gmail.com>
2023-10-04 11:40:07 -0400

    Shopping List

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Bread, Eggs, Water, Chicken, Injera, Beef, Peanut Butter, Coffee, Oranges, Chips");
+    }
+}
